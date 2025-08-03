3db40bba7cd368719f7c48fe488adbbf26229b64
Prakash <prakas.rj@gmail.com>
2025-08-03 13:33:36 -0500

    changed name of the bucket

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # backend.tf
 terraform {
   backend "s3" {
-    bucket         = "demo-S3bucket" # <-- TODO: Replace with your S3 bucket name
+    bucket         = "demo-prak23-S3bucket" # <-- TODO: Replace with your S3 bucket name
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
   }
