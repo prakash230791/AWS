2d8b531d2f2d422c65cbdbec032607979fbd4763
Prakash <prakas.rj@gmail.com>
2025-08-03 13:40:17 -0500

    Comit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # backend.tf
 terraform {
   backend "s3" {
-    bucket         = "demo-tf-979858933388-us-east-1" # <-- TODO: Replace with your S3 bucket name
+    bucket         = "tfstate-979858933388-us-east-1" # <-- TODO: Replace with your S3 bucket name
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
   }
